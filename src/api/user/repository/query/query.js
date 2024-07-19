794db970b5157d43623a2ea58bc35716de7a35b5
Rizky Naufal <naufalrizky79@gmail.com>
2024-07-19 13:57:14 +0700

    update query improvement

--- src/api/user/repository/query/query.js
+++ src/api/user/repository/query/query.js
@@ -2,6 +2,39 @@ const { ErrorHandler } = require("../../../../handler/error");
 const pg = require("../../../../config/db/db");
 class UserQuery {
   constructor() {}
+  async customGetUser(data) {
+    let sql = `SELECT * FROM user_tb  `;
+    let count = 0;
+    let value = [];
+    let conditions = [];
+
+    if (typeof data.email !== "undefined") {
+      if (count == 0) {
+        conditions.push(`WHERE email ILIKE  $${conditions.length + 1}`);
+      } else {
+        conditions.push(`email ILIKE  $${conditions.length + 1}`);
+      }
+      value.push(`%${data.email}%`);
+      count++;
+    }
+    if (typeof data.nama !== "undefined") {
+      if (count == 0) {
+        conditions.push(`WHERE nama ILIKE  $${conditions.length + 1}`);
+      } else {
+        conditions.push(`nama ILIKE  $${conditions.length + 1}`);
+      }
+      value.push(`%${data.nama}%`);
+      count++;
+    }
+    sql += conditions.join(" OR ");
+
+    const querySql = {
+      text: sql,
+      values: value,
+    };
+    var res = await pg.dbQuery(querySql);
+    return res[0];
+  }
   async getAllUser() {
     const query = `SELECT * FROM user_tb `;
     var res = await pg.dbQuery(query);
--- src/api/user/repository/query/query_handler.js
+++ src/api/user/repository/query/query_handler.js
@@ -45,38 +45,14 @@ class UserQueryHandler {
       throw new ErrorHandler.BadRequestError(error);
     } else {
       try {
-        let sql = `SELECT * FROM user_tb  `;
-        let count = 0;
-        let value = [];
-        let conditions = [];
-
-        if (typeof param.email !== "undefined") {
-          if (count == 0) {
-            conditions.push(`WHERE email ILIKE  $${conditions.length + 1}`);
-          } else {
-            conditions.push(`email ILIKE  $${conditions.length + 1}`);
-          }
-          value.push(`%${param.email}%`);
-          count++;
-        }
-        if (typeof param.nama !== "undefined") {
-          if (count == 0) {
-            conditions.push(`WHERE nama ILIKE  $${conditions.length + 1}`);
-          } else {
-            conditions.push(`nama ILIKE  $${conditions.length + 1}`);
-          }
-          value.push(`%${param.nama}%`);
-          count++;
-        }
-        sql += conditions.join(" OR ");
-
-        const querySql = {
-          text: sql,
-          values: value,
+        var response = await query.customGetUser(param);
+        return {
+          userId: response.user_id,
+          nama: response.nama,
+          email: response.email,
+          noHp: response.no_hp,
+          role: response.role,
         };
-
-        var response = await query.getUser();
-        return response;
       } catch (error) {
         throw new ErrorHandler.ServerError(error);
       }
