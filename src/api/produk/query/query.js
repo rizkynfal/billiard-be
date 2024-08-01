0ea9f15ea5325f7a6c849bb16aaff8c8619ef18f
Rizky Naufal <naufalrizky79@gmail.com>
2024-08-01 10:06:38 +0700

    update query

--- src/api/produk/query/query.js
+++ src/api/produk/query/query.js
@@ -8,6 +8,7 @@ class ProdukQuery {
     if (data && data.noMeja) {
       query += ` WHERE nama = '${"MEJA " + data.noMeja}'`;
     }
+    query += "ORDER BY product_id";
     const res = await pg.dbQuery(query);
     return res;
   }
